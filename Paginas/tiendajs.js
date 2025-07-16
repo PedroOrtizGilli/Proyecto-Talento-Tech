b818efb95dcd294e2f90efc98e343760e2359dcf
Pedro Ortiz Gilli <pedroortizgilli@hotmail.com>
2025-07-16 19:19:55 -0300

    Actualizacon 16/07/25

---
+++ Paginas/tiendajs.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMiniaturas = document.getElementById("mostrar-minis");
+    const btnAccesorios = document.getElementById("mostrar-accesorios");
+    const btnTodos = document.getElementById("mostrar-todos");
+
+    const productos = document.querySelectorAll(".en-muestra");
+
+    btnMiniaturas.addEventListener("click", (e) => {
+        e.preventDefault();
+        productos.forEach(p => {
+            if (p.classList.contains("en-muestra-miniatura")) {
+                p.style.display = "block";
+            } else {
+                p.style.display = "none";
+            }
+        });
+    });
+
+    btnAccesorios.addEventListener("click", (e) => {
+        e.preventDefault();
+        productos.forEach(p => {
+            if (p.classList.contains("en-muestra-accesorio")) {
+                p.style.display = "block";
+            } else {
+                p.style.display = "none";
+            }
+        });
+    });
+
+    btnTodos.addEventListener("click", (e) => {
+        e.preventDefault();
+        productos.forEach(p => {
+            p.style.display = "block";
+        });
+    });
+});
